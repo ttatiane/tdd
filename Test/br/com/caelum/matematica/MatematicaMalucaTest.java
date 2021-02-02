2e014c3daeae695417b5973392ba710e049ea2b8
ttatiane <49352132+ttatiane@users.noreply.github.com>
2021-02-02 11:53:29 -0300

    Teste da condicional de um método - caminhos possíveis de uma condicional

--- Test/br/com/caelum/matematica/MatematicaMalucaTest.java
+++ Test/br/com/caelum/matematica/MatematicaMalucaTest.java
@@ -1,4 +1,32 @@
+package br.com.caelum.matematica;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class MatematicaMalucaTest {
-  
+
+    @Test
+    public void deveMultiplicarNumerosMaioresQue30() {
+        //cenário: deve retornar valor maior que 30 * 4
+        int numero = 31;
+        //ação
+        MatematicaMaluca matematicaMaluca = new MatematicaMaluca();
+        //validação
+        assertEquals(30*4, matematicaMaluca.contaMaluca(numero));
+        System.out.println("Maior: 30x4 == " + matematicaMaluca.contaMaluca(numero));
+    }
+
+    @Test
+    public void deveMultiplicarNumerosMaioresQue10EMenoresQue30() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(20*3, matematica.contaMaluca(20));
+    }
+
+    @Test
+    public void deveMultiplicarNumerosMenoresQue10() {
+        MatematicaMaluca matematica = new MatematicaMaluca();
+        assertEquals(5*2, matematica.contaMaluca(5));
+    }
+
 }
