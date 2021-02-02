1fd9e13b9906b5fe8ef45f49efc5a7683be29ffe
ttatiane <49352132+ttatiane@users.noreply.github.com>
2021-02-02 14:33:04 -0300

    Testa restrição dos lances na  classe Leilao, via TDD e refatora

--- src/br/com/caelum/leilao/dominio/Leilao.java
+++ src/br/com/caelum/leilao/dominio/Leilao.java
@@ -6,20 +6,31 @@ import java.util.List;
 
 public class Leilao {
 
-	private String descricao;
-	private List<Lance> lances;
+    private String descricao;
+    private List<Lance> lances;
 
-	public Leilao(String descricao) {
-		this.descricao = descricao;
-		this.lances = new ArrayList<Lance>();
-	}
-	
-	public void propoe(Lance lance) {
-		lances.add(lance);
-	}
+    public Leilao(String descricao) {
+        this.descricao = descricao;
+        this.lances = new ArrayList<Lance>();
+    }
 
-	public List<Lance> getLances() {
-		return Collections.unmodifiableList(lances);
-	}
+    public void propoe(Lance lance) {
+        int total = 0;
+        for(Lance l : lances) {
+            if(l.getUsuario().equals(lance.getUsuario())) total++;
+        }
+
+        if (lances.isEmpty() || (!ultimoLanceDado().getUsuario().equals(lance.getUsuario()) && total < 5)) {
+            lances.add(lance);
+        }
+    }
+
+    private Lance ultimoLanceDado() {
+        return lances.get(lances.size() - 1);
+    }
+
+    public List<Lance> getLances() {
+        return Collections.unmodifiableList(lances);
+    }
 
 }
