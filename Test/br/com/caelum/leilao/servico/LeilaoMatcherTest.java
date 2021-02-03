d4829a12e1525c06730855dd6d89d1e2b6ec0bea
ttatiane <49352132+ttatiane@users.noreply.github.com>
2021-02-02 23:12:41 -0300

    Cria novo matcher

--- Test/br/com/caelum/leilao/servico/LeilaoMatcherTest.java
+++ Test/br/com/caelum/leilao/servico/LeilaoMatcherTest.java
@@ -1,4 +1,27 @@
-import static org.junit.Assert.*;
+package br.com.caelum.leilao.servico;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import org.junit.Test;
+
+import static br.com.caelum.leilao.servico.LeilaoMatcher.temUmLance;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 public class LeilaoMatcherTest {
-  
+
+    @Test
+    public void deveReceberUmLance() {
+        Leilao leilao = new CriadorDeLeilao().para("Macbook Pro 15").constroi();
+        assertEquals(0, leilao.getLances().size());
+
+        Lance lance = new Lance(new Usuario("Steve Jobs"), 2000);
+        leilao.propoe(lance);
+
+        assertThat(leilao.getLances().size(), equalTo(1));
+        assertThat(leilao, temUmLance(lance));
+    }
+
 }
